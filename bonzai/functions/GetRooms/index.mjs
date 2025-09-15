aaf900db54df2335fd0a344152cc44f7d6d2fb53
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-09-15 16:26:12 +0200

    Functions postRooms, getAllRooms and getAvailableRooms done.

--- bonzai/functions/GetRooms/index.mjs
+++ bonzai/functions/GetRooms/index.mjs
@@ -1,5 +1,10 @@
-import { sendResponses } from '../../responses/index.mjs';
+import middy from "@middy/core";
+import { sendResponses } from "../../responses/index.mjs";
+import { getAvailableRooms } from "../../services/rooms.mjs";
+import { errorHandler } from "../../middelwares/errorHandler.mjs";
 
-export const handler = async (event) => {
-	return sendResponses(200, { message: 'success' });
-};
+export const handler = middy(async (event) => {
+  const rooms = await getAvailableRooms();
+  if (rooms) return sendResponses(200, { message: "success", data: rooms });
+  else return sendResponses(404, { success: false, message: "Rooms could not be found." });
+}).use(errorHandler());
--- bonzai/services/rooms.mjs
+++ bonzai/services/rooms.mjs
@@ -1,21 +1,71 @@
-import { client } from './client.mjs';
-import { PutItemCommand } from '@aws-sdk/client-dynamodb';
-import { generateId } from '../utils/generateId.mjs';
-export const addRoom = async (room) => {
-	const command = new PutItemCommand({
-		TableName: 'bonzai-db',
-		Item: {
-			pk: { S: `${generateId('ROOM')}` },
-			sk: { S: `ROOM-${room.type}` },
-			createdAt: { S: new Date().toISOString() },
-		},
-	});
+import { client } from "./client.mjs";
+import { PutItemCommand, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { generateId } from "../utils/generateId.mjs";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 
-	try {
-		await client.send(command);
-		return true;
-	} catch (error) {
-		console.log('ERROR from rooms-db', error.message);
-		return false;
-	}
+export const addRoom = async (room) => {
+  console.log("room:", room);
+  const id = `ROOM#${generateId(room.type)}`;
+  console.log("id: ", id);
+
+  const command = new PutItemCommand({
+    TableName: "bonzai-db",
+    Item: {
+      pk: { S: "ROOM" },
+      sk: { S: id },
+      available: { BOOL: room.available },
+      beds: { N: room.beds.toString() },
+      price: { N: room.price.toString() },
+      createdAt: { S: new Date().toISOString() },
+    },
+  });
+
+  try {
+    await client.send(command);
+    return true;
+  } catch (error) {
+    console.log("ERROR from rooms-db", error.message);
+    return false;
+  }
+};
+
+export const getAllRooms = async () => {
+  const command = new QueryCommand({
+    TableName: "bonzai-db",
+    KeyConditionExpression: "pk= :pk AND begins_with(sk, :sk)",
+    ExpressionAttributeValues: {
+      ":pk": { S: "ROOM" },
+      ":sk": { S: "ROOM" },
+    },
+  });
+
+  try {
+    const { Items } = await client.send(command);
+    const rooms = Items.map((item) => unmarshall(item));
+    return rooms;
+  } catch (error) {
+    console.log("ERROR in db", error.message);
+    return false;
+  }
+};
+export const getAvailableRooms = async () => {
+  const command = new QueryCommand({
+    TableName: "bonzai-db",
+    KeyConditionExpression: "pk = :pk AND begins_with(sk, :sk)",
+    ExpressionAttributeValues: {
+      ":pk": { S: "ROOM" },
+      ":sk": { S: "ROOM" },
+      ":available": { BOOL: true },
+    },
+    FilterExpression: "available = :available",
+  });
+
+  try {
+    const { Items } = await client.send(command);
+    const rooms = Items.map((item) => unmarshall(item));
+    return rooms;
+  } catch (error) {
+    console.log("ERROR in db", error.message);
+    return false;
+  }
 };
