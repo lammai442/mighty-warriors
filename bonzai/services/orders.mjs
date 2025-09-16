cfb88e8389d6cb5e5e3abe3d6c8f744b64f70066
lammai442 <lam.mai442@gmail.com>
2025-09-16 10:36:29 +0200

    Added functions for getOrders and getOrderById with validation

--- bonzai/services/orders.mjs
+++ bonzai/services/orders.mjs
@@ -1,12 +1,44 @@
 import { client } from './client.mjs';
-import { GetItemCommand } from '@aws-sdk/client-dynamodb';
-
-export const getOrders = (orderId) => {
-  const command = GetItemCommand({
+import { QueryCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
+export const getAllOrders = async () => {
+  const command = new QueryCommand({
     TableName: 'bonzai-db',
-    Key: {
-        pk: 'ORDER',
-        sk: 
-    }
+    KeyConditionExpression: 'pk= :pk AND begins_with(sk, :sk)',
+    ExpressionAttributeValues: {
+      ':pk': { S: 'ORDER' },
+      ':sk': { S: 'ORDER#' },
+    },
   });
+
+  try {
+    const data = await client.send(command);
+    if (!data.Items || data.Items.length === 0) return [];
+
+    const orders = data.Items.map((item) => unmarshall(item));
+    return orders;
+  } catch (error) {
+    console.log('ERROR in orders-db', error.message);
+    return false;
+  }
+};
+
+export const getOneOrder = async (orderId) => {
+  const command = new QueryCommand({
+    TableName: 'bonzai-db',
+    KeyConditionExpression: 'pk= :pk AND begins_with(sk, :sk)',
+    ExpressionAttributeValues: {
+      ':pk': { S: 'ORDER' },
+      ':sk': { S: `ORDER#${orderId}` },
+    },
+  });
+
+  try {
+    const { Items } = await client.send(command);
+    const orders = Items.map((item) => unmarshall(item));
+    return orders;
+  } catch (error) {
+    console.log('ERROR in orders-db', error.message);
+    return false;
+  }
 };
