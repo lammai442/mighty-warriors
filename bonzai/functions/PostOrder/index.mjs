e85d37e5a8f6dc7bd952e577be7a45e0b53453a5
StefanMogren <stefan_mogren@hotmail.com>
2025-09-16 11:43:10 +0200

    added integer to numbers in orderSchema, finished check against room availability in PostOrder

--- bonzai/functions/PostOrder/index.mjs
+++ bonzai/functions/PostOrder/index.mjs
@@ -3,13 +3,38 @@ import middy from '@middy/core';
 import { errorHandler } from '../../middelwares/errorHandler.mjs';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import { validateOrder } from '../../middelwares/validateOrder.mjs';
+import { getAllRooms } from '../../services/rooms.mjs';
 
 export const handler = middy(async (event) => {
-  const newOrder = event.body;
+  const orderRequest = event.body;
+  const allRooms = await getAllRooms();
 
-  // Need to get current rooms. Need to run GET on /api/rooms
+  const orderRooms = [];
 
-  // Verification whether all rooms are available
+  // Loopar igenom alla rumsID som skickas med i body
+  for (const roomId of orderRequest.rooms) {
+    // Letar upp det efterfrågade rummet
+    const room = allRooms.find((r) => r.sk.includes(roomId));
+
+    // Om rummet inte finns så returneras 404
+    if (room === undefined) {
+      return sendResponses(404, {
+        message: `Room with ID: ${roomId} doesn't exist`,
+      });
+
+      // Om rummet finns men är otillgängligt
+    } else if (room.available === false) {
+      return sendResponses(503, {
+        message: `Room with ID: ${roomId} is unavailable`,
+      });
+
+      // Om rummet finns och är tillgängligt
+    } else {
+      orderRooms.push(room);
+    }
+  }
+
+  // ----- -----
 
   // Verification against number of guests versus number of beds
 
@@ -19,7 +44,7 @@ export const handler = middy(async (event) => {
 
   return sendResponses(201, {
     message: 'Successfully created order',
-    newOrder,
+    orderRooms,
   });
 })
   .use(httpJsonBodyParser())
--- bonzai/models/orderSchema.mjs
+++ bonzai/models/orderSchema.mjs
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 export const orderSchema = Joi.object({
-  guests: Joi.number().min(1).required(),
+  guests: Joi.number().integer().min(1).required(),
   rooms: Joi.array().items(Joi.string().required()),
-  nights: Joi.number().min(1).required(),
+  nights: Joi.number().integer().min(1).required(),
   name: Joi.string().required(),
   email: Joi.string().required(),
 });
