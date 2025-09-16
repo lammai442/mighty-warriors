a41a739efa12421e64e1fa594d9b24186d8c7bb4
StefanMogren <stefan_mogren@hotmail.com>
2025-09-16 09:29:46 +0200

    added two extra schema checks in orderSchema, added comments on what needs to be done in PostOrder

--- bonzai/functions/PostOrder/index.mjs
+++ bonzai/functions/PostOrder/index.mjs
@@ -6,6 +6,17 @@ import { validateOrder } from '../../middelwares/validateOrder.mjs';
 
 export const handler = middy(async (event) => {
   const newOrder = event.body;
+
+  // Need to get current rooms. Need to run GET on /api/rooms
+
+  // Verification whether all rooms are available
+
+  // Verification against number of guests versus number of beds
+
+  // Adjust the rooms' availability "true/false"
+
+  // Create a new order with unique orderId
+
   return sendResponses(201, {
     message: 'Successfully created order',
     newOrder,
--- bonzai/models/orderSchema.mjs
+++ bonzai/models/orderSchema.mjs
@@ -4,4 +4,6 @@ export const orderSchema = Joi.object({
   guests: Joi.number().min(1).required(),
   rooms: Joi.array().items(Joi.string().required()),
   nights: Joi.number().min(1).required(),
+  name: Joi.string().required(),
+  email: Joi.string().required(),
 });
